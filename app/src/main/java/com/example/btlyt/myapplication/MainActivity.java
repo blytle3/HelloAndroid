eb7086832e296f0d417859940443e0a1692402b6
Brad Lytle <btlytle@gmail.com>
2018-11-03 20:09:16 -0500

    Got the emulator working

--- app/src/main/java/com/example/btlyt/myapplication/MainActivity.java
+++ app/src/main/java/com/example/btlyt/myapplication/MainActivity.java
@@ -1,13 +1,26 @@
-package com.example.btlyt.myapplication;
+ package com.example.btlyt.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView firstTextView = (TextView)findViewById(R.id.textView4);
+        Button firstButton = (Button)findViewById(R.id.button_1);
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstTextView.setText("We did the thing!");
+            }
+        });
     }
+
 }
