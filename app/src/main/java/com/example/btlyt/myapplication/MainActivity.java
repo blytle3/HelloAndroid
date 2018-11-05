d994e800dacb82b4b465d8011adf6350e62e39c3
Brad Lytle <btlytle@gmail.com>
2018-11-04 22:18:35 -0500

    Added second activity

--- app/src/main/java/com/example/btlyt/myapplication/MainActivity.java
+++ app/src/main/java/com/example/btlyt/myapplication/MainActivity.java
@@ -1,12 +1,14 @@
  package com.example.btlyt.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
  public class MainActivity extends AppCompatActivity {
+
+     public static final String EXTRA_MESSAGE = "com.example.btlyt.myapplication.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,4 +16,11 @@ import android.widget.TextView;
         setContentView(R.layout.activity_main);
     }
 
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }
